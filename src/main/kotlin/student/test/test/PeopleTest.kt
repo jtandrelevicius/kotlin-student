e4d33e9173f31c1de3edc967ae44cf7e10f7fd14
jefferson <jtandrelevicius@gmail.com>
2021-08-25 18:49:38 -0400

    deleted peopletest class

--- src/main/kotlin/student/test/test/PeopleTest.kt
+++
@@ -1,9 +0,0 @@
-package student.test.test
-
-import student.test.People
-
-fun main() {
-    val jeff = People(name = "Jefferson tiago", cpf = "000.200.200-56")
-    println(jeff.name)
-    println(jeff.cpf)
-}
