5a6c69dae3fbe3f5f8704f8d1e2c2f24bad9a4b4
jefferson <jtandrelevicius@gmail.com>
2021-08-25 14:07:35 -0400

    upgrade commit

--- src/main/kotlin/student/test/People.kt
+++ src/main/kotlin/student/test/People.kt
@@ -3,6 +3,10 @@ package student.test
 class People {
     var nome : String = "Jefferson Tiago"
     var cpf : String = "000.233.000-33"
+
+    inner  class Endereco{
+        var rua : String = "Rua teste"
+    }
 }
 
 fun main() {
@@ -10,4 +14,5 @@ fun main() {
 
     println(jeff.nome)
     println(jeff.cpf)
+    println(jeff.Endereco().rua)
 }
