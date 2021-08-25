c9adf17f635dfc905b092997c0c4c6aa1706aa33
jefferson <jtandrelevicius@gmail.com>
2021-08-25 17:30:18 -0400

    updated people class

--- src/main/kotlin/student/test/People.kt
+++ src/main/kotlin/student/test/People.kt
@@ -1,28 +1,25 @@
 package student.test
 
-class People {
-    var nome : String = "Jefferson Tiago"
+open class People (
+    open val name: String,
+    open val cpf: String
+    )
+
+
+    //var nome : String = "Jefferson Tiago"
 
     //DEIXA A PROPRIEDADE SET PRIVADA ASSIM NAO SERA ALTARADO MAIS APOS CRIAR A INSTACIA
-    var cpf : String = "000.233.000-33"
-    private set
+    //var cpf : String = "000.233.000-33"
+    //private set
 
     //RECURSO INNER CLASS CRIA UMA CLASSE INTERNA
     //inner  class Endereco{
         //var rua : String = "Rua teste"
     //}
 
-    constructor()
+    //constructor()
     //FUNCTION UNINDO NOME E CPF UTILIZANDO O $ INTERPOLACAO
-    fun peopleInfo() = "$nome - $cpf"
-}
+    //fun peopleInfo() = "$nome - $cpf"
 
-fun main() {
-    val jeff = People()
 
-    //println(jeff.nome)
-    //println(jeff.cpf)
-    //println(jeff.Endereco().rua)
 
-    println(jeff.peopleInfo())
-}
