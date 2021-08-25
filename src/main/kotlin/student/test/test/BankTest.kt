c8730d9aedcbcd0ee8d7908cdd8a0fe19f3befd9
jefferson <jtandrelevicius@gmail.com>
2021-08-25 16:50:59 -0400

    created banktest class

---
+++ src/main/kotlin/student/test/test/BankTest.kt
@@ -0,0 +1,10 @@
+package student.test.test
+
+import student.test.Bank
+
+fun main() {
+    val digOneBank = Bank(nome="DigitalOne", numero=15 )
+
+    println(digOneBank.nome)
+    println(digOneBank.numero)
+}
