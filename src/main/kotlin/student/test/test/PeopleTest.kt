50aeaa333d2c1e40103a1515762793ac4f69df83
jefferson <jtandrelevicius@gmail.com>
2021-08-25 17:30:36 -0400

    created peopletest class

---
+++ src/main/kotlin/student/test/test/PeopleTest.kt
@@ -0,0 +1,14 @@
+package student.test.test
+
+import student.test.People
+
+fun main() {
+    val jeff = People()
+
+    //println(jeff.nome)
+    //println(jeff.cpf)
+    //println(jeff.Endereco().rua)
+
+    println(jeff.peopleInfo())
+
+}
